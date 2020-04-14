e82bd7bfd4c71d218dadbf9af2ea4c3bd61e2507
eversecer <eversecer@eversec.cn>
2020-04-14 21:53:58 +0800

    remove email notify from build plugin.

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -2,6 +2,6 @@ def call(Map config) {
     node {
         git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
         sh 'mvn install -DskipTests'
-        mail to: '...', subject: "${config.name} plugin build", body: '...'
+        //mail to: '...', subject: "${config.name} plugin build", body: '...'
     }
 }
