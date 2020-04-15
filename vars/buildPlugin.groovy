bc69c224425721405edac257796a7c94236c6fc0
eversecer <eversecer@eversec.cn>
2020-04-15 09:57:12 +0800

    remove pipeline from build plugin.

--- vars/buildPlugin.groovy
+++ vars/buildPlugin.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
-    node {
+    //node {
         git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
         sh 'mvn install -DskipTests'
         //mail to: '...', subject: "${config.name} plugin build", body: '...'
-    }
+    //}
 }
